ffbe0aa76180318cd721fa855e69c0043557e296
Diivvuu <divyanshubawa9@gmail.com>
2024-07-19 00:43:55 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import { Accordion } from "./components/ui/accordion";
+import { Button } from "./components/ui/button";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      {/* <Accordion /> */}
+      <Button></Button>
+      {/* hi */}
+    </>
+  );
+}
+
+export default App;
